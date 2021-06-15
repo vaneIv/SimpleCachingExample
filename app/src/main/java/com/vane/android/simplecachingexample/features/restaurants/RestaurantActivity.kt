3ddf020b00e0c2d4d8a6a33b2f6cd597c241862e
ivVane <iv.vane@gmail.com>
2021-06-15 21:29:48 +0200

    Creating the recycler view and adapter

---
+++ app/src/main/java/com/vane/android/simplecachingexample/features/restaurants/RestaurantActivity.kt
@@ -0,0 +1,12 @@
+package com.vane.android.simplecachingexample.features.restaurants
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.vane.android.simplecachingexample.R
+
+class RestaurantActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_restaurant)
+    }
+}
